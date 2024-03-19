e4e83cb763249c9bf5ca0eeeebb550b1e5f0b188
Stepan <cholakstepan@gmail.com>
2024-03-19 10:13:43 +0200

    Initial commit

---
+++ Solution1/Domen/Entities/BaseEntity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Domen.Entities.Pers;
+
+namespace Domen.Entities
+{
+    /// <summary>
+    /// Базовый класс для всех сущностей
+    /// </summary>
+    public abstract class BaseEntity
+    {
+        /// <summary>
+        /// Универсальный идентификатор сущностей
+        /// </summary>
+        protected Guid Id { get; set; }
+        /// <summary>
+        /// Дата создания сущностей
+        /// </summary>
+        protected DateTime CreatedData { get; set; }
+        /// <summary>
+        /// Имя, Фамилия, Дата рождения и пол персоны
+        /// </summary>
+        protected string FirstName { get; set; }
+        protected string LastName { get; set; }
+        protected DateTime DateOfBirth { get; set; }
+        protected Gender UserGender { get; set; }
+        public enum Gender
+        {
+            Male,
+            Female,
+            Other
+        }
+        /// <summary>
+        /// Метод для сравнения сущностей по айди
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            // Проверка на null и сравнение типов
+            if (obj is not BaseEntity entity)
+                return false;
+
+            // Сравнение идентификаторов
+            return Id == entity.Id;
+        }
+        /// <summary>
+        /// GetHashCode теперь возвращает хэш-код сущности
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+             return Id.GetHashCode();
+        }
+
+        /// <summary>
+        /// To Do Домашнее задание. Почему студия заставила переопределить GetHashCode. Перезаписать GetHashCode & Equals* 
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="NotImplementedException"></exception>
+    }
+}
---
+++ Solution1/Domen/Entities/Pers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domen.Entities
+{
+    /// <summary>
+    /// Основная сущность персоны(человека)
+    /// </summary>
+    public class Pers: BaseEntity
+    {
+        /// <summary>
+        /// To Do. Максимально полно описать сущность персоны. Помимо описания сделать все поля через конструктор.
+        /// </summary>
+        public Pers(string firstName, string lastName, DateTime dateOfBirth, Gender gender, DateTime createdData) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = dateOfBirth;
+            UserGender = gender;
+            CreatedData = createdData;
+        }
+        public static Pers ExampleUsage(string firstName, string lastName, DateTime dateOfBirth, Gender gender, DateTime createdData)
+        {            // Создание объекта Pers с помощью конструктора
+
+            return new Pers(firstName, lastName, dateOfBirth, gender, createdData);
+        }
+    }
+
+}
